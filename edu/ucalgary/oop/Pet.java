40695a74d80b9730feb15cdfb32c1bf259c361ea
Youssef Salem <156625440+yssalem@users.noreply.github.com>
2025-08-16 15:20:46 -0600

    Finished Pet.java except owner member

--- edu/ucalgary/oop/Pet.java
+++ edu/ucalgary/oop/Pet.java
@@ -1,3 +1,21 @@
 public abstract class Pet {
+    private String name;
+    private int age;
     
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
 }
