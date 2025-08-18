d009ee9e54609d6d55f6049738d7ce332caf2c72
noor055 <noorsheikh055@gmail.com>
2025-08-18 12:47:40 -0600

    added functions to Appointment

--- edu/ucalgary/oop/Veterinarian.java
+++ edu/ucalgary/oop/Veterinarian.java
@@ -1,5 +1,7 @@
 package edu.ucalgary.oop;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Veterinarian extends Employee{
@@ -14,5 +16,22 @@ public class Veterinarian extends Employee{
     }
     public void setSpecialization(String specialization) {
         this.specialization = specialization;
+    }
+    public boolean checkIfFree(LocalDateTime date){
+        for(Appointment appointment : appointmentSchedule){
+            if(appointment.getDate().equals(date)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int checkNumberAppointments(LocalDateTime date){
+        int dateCount = 0;
+        for(Appointment appointment : appointmentSchedule){
+            if(appointment.getDate().toLocalDate().equals(date.toLocalDate())){
+                dateCount++;
+            }
+        }
+        return dateCount;
     }
 }
