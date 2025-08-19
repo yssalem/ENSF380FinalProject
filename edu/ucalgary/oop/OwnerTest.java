332787195ea1d633423aa4bbd68e7413e16c516c
Youssef Salem <156625440+yssalem@users.noreply.github.com>
2025-08-18 22:30:28 -0600

    altered owner constructor test

--- edu/ucalgary/oop/OwnerTest.java
+++ edu/ucalgary/oop/OwnerTest.java
@@ -7,11 +7,7 @@ public class OwnerTest {
     
     @Test
     public void testOwnerConstructor() {
-        Owner expResultObject = new Owner();
-        expResultObject.setName("John Doe");
-        expResultObject.setEmail("john.doe@email.com");
-        expResultObject.setPhoneNumber("555-5555");
-        String[] expResult = {expResultObject.getName(), expResultObject.getEmail(), expResultObject.getPhoneNumber()};
+        String[] expResult = {"John Doe", "john.doe@email.com", "555-5555"};
 
         Owner resultObject = new Owner("John Doe", "john.doe@email.com", "555-5555");
         String[] result = {resultObject.getName(), resultObject.getEmail(), resultObject.getPhoneNumber()};
